45488db980c313ae525837079bc2aa09f191d33a
sheep737 <ornament.broom@gmail.com>
2025-12-20 22:34:37 +0900

    Create game.js

---
+++ game.js
@@ -0,0 +1,6 @@
+const CARD_POOL = [
+  { name:"Fire", type:"attack", power:3, cost:1 },
+  { name:"Heal", type:"heal", power:3, cost:2 },
+  { name:"Draw", type:"draw", power:1, cost:1 },
+  { name:"Soldier", type:"unit", atk:2, hp:2, cost:2 }
+];
